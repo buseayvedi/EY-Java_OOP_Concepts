cc5991a9ebdf7a7fe2887bf5ae2380dc7c96fb69
European01 <emiryaziciofficial@gmail.com>
2024-01-20 14:39:52 -0500

    Made Toyota class as a child of Car class.

--- src/inheritance/VehicleTask/Toyota.java
+++ src/inheritance/VehicleTask/Toyota.java
@@ -1,4 +1,8 @@
 package inheritance.VehicleTask;
 
-public class Toyota {
+public class Toyota extends Car{
+
+    public Toyota(String brand, String model, String color, int year, double price) {
+        super(brand, model, color, year, price);
+    }
 }
