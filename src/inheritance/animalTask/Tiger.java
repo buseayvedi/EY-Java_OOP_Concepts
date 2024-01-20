03139c5a899346c6ff1ee607a17c34ee585519df
European01 <emiryaziciofficial@gmail.com>
2024-01-20 01:43:35 -0500

    Made the Tiger class child of Animal class and added a method.

--- src/inheritance/animalTask/Tiger.java
+++ src/inheritance/animalTask/Tiger.java
@@ -1,4 +1,8 @@
 package inheritance.animalTask;
 
-public class Tiger {
+public class Tiger extends Animal{
+
+    public void hunt(){
+        System.out.println(getName()+" is hunting");
+    }
 }
