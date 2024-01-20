73ef434758d0cf5244d21e5fbb956ed2ba63ecea
European01 <emiryaziciofficial@gmail.com>
2024-01-20 02:07:10 -0500

    Made Teacher class as child of Employee class.

--- src/inheritance/phoneTask/Teacher.java
+++ src/inheritance/phoneTask/Teacher.java
@@ -1,4 +1,14 @@
 package inheritance.phoneTask;
 
-public class Teacher {
+import java.time.LocalDate;
+
+public class Teacher extends Employee{
+
+    public Teacher(String name, char gender, LocalDate dateOfBirth, String employeeId, String jobTitle, double salary) {
+        super(name, gender, dateOfBirth, employeeId, jobTitle, salary);
+    }
+
+    public void teach(){
+        System.out.println(getName()+" is teaching");
+    }
 }
