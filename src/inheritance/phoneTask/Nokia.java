cd485ffeee811e5499d0a7963dfc22e6f71ce8d1
European01 <emiryaziciofficial@gmail.com>
2024-01-20 02:51:30 -0500

    Made Nokia class as a child of Phone class and added a method.

--- src/inheritance/phoneTask/Nokia.java
+++ src/inheritance/phoneTask/Nokia.java
@@ -1,4 +1,13 @@
 package inheritance.phoneTask;
 
-public class Nokia {
+public class Nokia extends Phone{
+
+    public Nokia(String model, String size, String color, int price) {
+        super("Nokia", model, size, color, price);
+    }
+
+    public void selfDefence(){
+        System.out.println(getBrand()+" "+getModel()+" can be used for self defense");
+    }
+
 }
