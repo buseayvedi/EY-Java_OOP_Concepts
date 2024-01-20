3f9b04ad8601b20b9216105177a5b24f307027bf
European01 <emiryaziciofficial@gmail.com>
2024-01-20 14:19:20 -0500

    Wrote some codes to test Animal objects.

--- src/inheritance/animal_MethodOverriding/TestAnimalObjects.java
+++ src/inheritance/animal_MethodOverriding/TestAnimalObjects.java
@@ -1,4 +1,34 @@
 package inheritance.animal_MethodOverriding;
 
 public class TestAnimalObjects {
+
+    public static void main(String[] args) {
+
+        Cat cat = new Cat("Bengle","Emily","Gray","Small",4,'F');
+        Dog dog = new Dog("Husky","Max","White","Large",3,'M');
+        Lion lion = new Lion("African Lion","Simba","Yellow","Large",6,'M',true);
+        Eagle eagle = new Eagle("African Eagle","Bella","Black & White","Medium",4,'F');
+
+        System.out.println(cat);
+        System.out.println(dog);
+        System.out.println(lion);
+        System.out.println(eagle);
+
+        System.out.println("------------------------------------------------------------------------");
+
+        cat.eat();
+        dog.eat();
+        lion.eat();
+        eagle.eat();
+
+        System.out.println("--------------------------------------------------------------------------");
+
+        cat.sleep();
+        dog.sleep();
+        lion.sleep();
+        eagle.sleep();
+
+        System.out.println("----------------------------------------------------------------------------");
+
+    }
 }
