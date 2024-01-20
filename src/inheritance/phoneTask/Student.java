fce20a75890e38e29bf7276acf51863094c2a1a7
European01 <emiryaziciofficial@gmail.com>
2024-01-20 02:05:26 -0500

    Made Student class as child of Person class and added more variables and methods.

--- src/inheritance/phoneTask/Student.java
+++ src/inheritance/phoneTask/Student.java
@@ -1,4 +1,35 @@
 package inheritance.phoneTask;
 
-public class Student {
+import java.time.LocalDate;
+
+public class Student extends Person{
+
+    private char grade;
+    private String studentId;
+
+    public Student(String name, char gender, LocalDate dateOfBirth, char grade, String studentId) {
+        super(name, gender, dateOfBirth);
+        setGrade(grade);
+        setStudentId(studentId);
+    }
+
+    public char getGrade() {
+        return grade;
+    }
+
+    public void setGrade(char grade) {
+        this.grade = grade;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public void study(){
+        System.out.println(getName()+" is studying");
+    }
 }
