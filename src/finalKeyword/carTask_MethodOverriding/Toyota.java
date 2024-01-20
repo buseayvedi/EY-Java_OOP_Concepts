69262a7d8047ff7ff3bed3a60afa542be09385f4
European01 <emiryaziciofficial@gmail.com>
2024-01-20 14:59:23 -0500

    Made Toyota class as a child of Car class.

--- src/finalKeyword/carTask_MethodOverriding/Toyota.java
+++ src/finalKeyword/carTask_MethodOverriding/Toyota.java
@@ -1,4 +1,8 @@
 package finalKeyword.carTask_MethodOverriding;
 
-public class Toyota {
+public class Toyota extends Car{
+
+    public Toyota(String model, String color, int year, double price) {
+        super(model, color, year, price);
+    }
 }
