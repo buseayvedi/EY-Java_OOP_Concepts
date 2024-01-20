519a5a02ba71388a450d298d8d131e95982ba9ac
European01 <emiryaziciofficial@gmail.com>
2024-01-20 14:43:24 -0500

    Added some codes to explain how field hiding works.

--- src/inheritance/FieldHiding.java
+++ src/inheritance/FieldHiding.java
@@ -1,4 +1,17 @@
 package inheritance;
 
-public class FieldHiding {
+class A {
+
+    public int a = 100 ;
+
+}
+public class FieldHiding  extends A{
+
+    public int a = 300;
+
+    public static void main(String[] args) {
+
+
+        System.out.println(new FieldHiding().a);
+    }
 }
