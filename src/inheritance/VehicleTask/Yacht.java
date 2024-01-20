20fd5dd0fff96b6e50b600d0bf34b8af2cb4fd82
European01 <emiryaziciofficial@gmail.com>
2024-01-20 14:37:33 -0500

    Made Yacht class as a child of Vehicle class .

--- src/inheritance/VehicleTask/Yacht.java
+++ src/inheritance/VehicleTask/Yacht.java
@@ -1,4 +1,8 @@
 package inheritance.VehicleTask;
 
-public class Yacht {
+public class Yacht extends Vehicle{
+
+    public Yacht(String brand, String model, String color, int year, double price) {
+        super(brand, model, color, year, price);
+    }
 }
