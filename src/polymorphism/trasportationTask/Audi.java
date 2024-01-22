390f630358ab385efc5f749184490e84c470eb66
European01 <emiryaziciofficial@gmail.com>
2024-01-21 22:17:19 -0500

    Make Audi class as a child of Car class and implement AutoPark interface. Override some methods.

--- src/polymorphism/trasportationTask/Audi.java
+++ src/polymorphism/trasportationTask/Audi.java
@@ -1,4 +1,23 @@
 package polymorphism.trasportationTask;
 
-public class Audi {
+public class Audi extends Car implements AutoPark{
+
+    public Audi(String make, String model, String color, int year, int price) {
+        super(make, model, color, year, price);
+    }
+
+    @Override
+    public void autoPark() {
+
+    }
+
+    @Override
+    public void transportPeople() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
 }
