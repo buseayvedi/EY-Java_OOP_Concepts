de88205333e3ebda63b46f8a64f139df936b22c1
European01 <emiryaziciofficial@gmail.com>
2024-01-21 22:14:44 -0500

    Make Plane class abstract and as a child of Transportation class.Implement Flyable interface and add a method.Override a method as well.

--- src/polymorphism/trasportationTask/Plane.java
+++ src/polymorphism/trasportationTask/Plane.java
@@ -1,4 +1,19 @@
 package polymorphism.trasportationTask;
 
-public class Plane {
+import abstraction.animalTask.Flyable;
+
+public abstract class Plane extends Transportation implements Flyable {
+
+    public Plane(String make, String model, String color, int year, int price) {
+        super(make, model, color, year, price);
+    }
+
+    public void land(){
+        System.out.println(getMake()+" "+getModel()+" is landing");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println(getMake()+" "+getModel()+" is flying");
+    }
 }
