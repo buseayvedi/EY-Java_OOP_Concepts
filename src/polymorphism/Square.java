e07f5c5b383e96fc7647cfe37a3fc632100f729a
European01 <emiryaziciofficial@gmail.com>
2024-01-21 22:43:14 -0500

    Add an example to practice.

--- src/polymorphism/Square.java
+++ src/polymorphism/Square.java
@@ -1,4 +1,23 @@
 package polymorphism;
 
 public class Square {
+
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Square){
+            if (((Square) obj).side == side){
+                return true;
+            }
+        }else{
+            System.err.println("Invalid object");
+            System.exit(1);
+        }
+        return false;
+    }
 }
