fbb4bc6281bce513db4a11900a7bf37989b5c5c5
European01 <emiryaziciofficial@gmail.com>
2024-01-21 22:12:37 -0500

    Make Car class abstract and as a child of Transportation class. Add a method.

--- src/polymorphism/trasportationTask/Car.java
+++ src/polymorphism/trasportationTask/Car.java
@@ -1,4 +1,12 @@
 package polymorphism.trasportationTask;
 
-public class Car {
+public abstract class Car extends Transportation{
+
+    public Car(String make, String model, String color, int year, int price) {
+        super(make, model, color, year, price);
+    }
+
+    public void drive(){
+        System.out.println("Driving "+getMake()+" "+getModel());
+    }
 }
