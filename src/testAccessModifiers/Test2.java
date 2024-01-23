4f8b87fc2c84238ebbc0b8a576f240fae0442578
European01 <emiryaziciofficial@gmail.com>
2024-01-23 16:53:13 -0500

    Add some examples to test how access modifiers work outside the package.

--- src/testAccessModifiers/Test2.java
+++ src/testAccessModifiers/Test2.java
@@ -1,4 +1,27 @@
 package testAccessModifiers;
 
+import static accessModifiers.Data.*;
+
 public class Test2 {
+
+    public static void main(String[] args) {
+
+        System.out.println(d);
+
+        method3();
+
+        System.out.println(e);
+        System.out.println(f);
+
+        method4();
+/*
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        method1();
+        method2();
+
+ */
+    }
 }
