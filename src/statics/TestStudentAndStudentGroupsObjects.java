29098049e77d97ee3463af74bc3c64d2dec3f149
European01 <emiryaziciofficial@gmail.com>
2024-01-23 15:28:38 -0500

    Add some examples to test Student and StudentGroups objects.

--- src/statics/TestStudentAndStudentGroupsObjects.java
+++ src/statics/TestStudentAndStudentGroupsObjects.java
@@ -1,4 +1,31 @@
 package statics;
 
 public class TestStudentAndStudentGroupsObjects {
+
+    public static void main(String[] args) {
+
+        Student student1 = new Student("Yusuf",21,'M',"A16");
+        Student student2 = new Student("Necla",43,'F',"B34");
+        Student student3 = new Student("Arif",27,'F',"C56");
+        Student student4 = new Student("Zuleyha",61,'F',"D43");
+        Student student5 = new Student("Kebire",55,'F',"E27");
+
+        Student[] students = {student2,student3,student4,student5};
+
+        StudentsGroups group1 = new StudentsGroups("Java Turtles",1);
+
+        group1.addStudent(student1);
+        group1.addStudent(students);
+
+        group1.addStudent("Khashayar",34,'M',"F35");
+
+        group1.removeStudent("D43");
+        group1.removeStudent("E27");
+
+        System.out.println(group1);
+
+        for (Student each : group1.students) {
+            System.out.println(each.name+ " : "+each.id);
+        }
+    }
 }
