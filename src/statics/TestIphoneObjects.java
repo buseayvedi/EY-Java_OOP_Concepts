e358ef4d228176adc16de06b5846c64306cc466c
European01 <emiryaziciofficial@gmail.com>
2024-01-23 15:26:04 -0500

    Add an example to test Iphone objects.

--- src/statics/TestIphoneObjects.java
+++ src/statics/TestIphoneObjects.java
@@ -1,4 +1,25 @@
 package statics;
 
 public class TestIphoneObjects {
+
+    public static void main(String[] args) {
+
+        Iphone iPhone = new Iphone("iPhone12","Black",1000);
+
+        System.out.println(iPhone.model);
+        System.out.println(iPhone.color);
+        System.out.println(iPhone.price);
+
+        iPhone.printPhoneInfo();
+/*
+        System.out.println(iPhone.brand);
+        System.out.println(iPhone.os);
+        iPhone.printOperatingSystem();
+
+ */
+
+        System.out.println(Iphone.brand);
+        System.out.println(Iphone.os);
+        Iphone.printOperatingSystem();
+    }
 }
