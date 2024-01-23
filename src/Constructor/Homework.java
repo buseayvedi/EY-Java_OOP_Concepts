7ea1d201948b66c1dfcd69d5f90326a38bddf3d9
European01 <emiryaziciofficial@gmail.com>
2024-01-23 14:47:34 -0500

    Add an example to practice.

--- src/Constructor/Homework.java
+++ src/Constructor/Homework.java
@@ -1,4 +1,18 @@
 package Constructor;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Homework {
+
+    public static void main(String[] args) {
+
+        LocalDateTime time = LocalDateTime.of(2020,11,24,13,0);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("EEEE, h:mm a, MMM/dd/y");
+        System.out.println(time.format(dtf));
+    }
 }
+/*
+1. use the LocalDateTime get the date and time in the following format:
+            Tuesday, 1:00 pm, Nov/24/2020
+ */
