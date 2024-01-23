94ca0a578170e88a9443b28224467f6b6665b864
European01 <emiryaziciofficial@gmail.com>
2024-01-23 15:38:49 -0500

    Add an example to explain how instance block works.

--- src/accessModifiers/InstanceBlock.java
+++ src/accessModifiers/InstanceBlock.java
@@ -1,4 +1,20 @@
 package accessModifiers;
 
 public class InstanceBlock {
+
+    {
+        System.out.println("Instance Block");
+
+    }
+
+    public InstanceBlock(){
+        System.out.println("Constructor");
+    }
+
+    public static void main(String[] args) {
+
+        new InstanceBlock();
+        new InstanceBlock();
+        new InstanceBlock();
+    }
 }
