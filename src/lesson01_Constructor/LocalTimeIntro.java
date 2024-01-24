a40598d51020953234bcdc92018ff1747641a25c
European01 <emiryaziciofficial@gmail.com>
2024-01-24 12:25:27 -0500

    Add some examples to practice.

--- src/lesson01_Constructor/LocalTimeIntro.java
+++ src/lesson01_Constructor/LocalTimeIntro.java
@@ -1,4 +1,32 @@
 package lesson01_Constructor;
 
+import java.time.LocalTime;
+
 public class LocalTimeIntro {
+
+    public static void main(String[] args) {
+
+        LocalTime starting_time = LocalTime.of(10,30);
+
+        System.out.println(starting_time);
+
+        LocalTime now = LocalTime.now();
+
+        System.out.println(now);
+
+        System.out.println("-------------------------------------------------------------------------");
+
+        LocalTime time1 = LocalTime.of(23,59,56,235624552);
+
+        System.out.println(time1);
+
+        time1 = time1.plusHours(1);
+
+        System.out.println(time1);
+
+        time1 = time1.plusMinutes(45);
+
+        System.out.println(time1);
+
+    }
 }
